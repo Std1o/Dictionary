1ce37f38c72f09c1af080416bd853217e6ceeb08
Std1o <37378410+Std1o@users.noreply.github.com>
2021-02-08 23:27:58 +0300

    recycler view

--- app/src/main/java/com/stdio/dictionary/MainActivity.java
+++ app/src/main/java/com/stdio/dictionary/MainActivity.java
@@ -1,19 +1,26 @@
 package com.stdio.dictionary;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ArrayList<DataModel> dataList;
+    private RecyclerView rv;
     SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initRecyclerView();
         DataBaseHelper databaseHelper = new DataBaseHelper(getApplicationContext());
         databaseHelper.create_db();
         db = databaseHelper.open();
@@ -21,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void readDb() {
+        dataList = new ArrayList<>();
         Cursor cursor = db.query("fraze", null, null, null, null, null, null);
 
         if (cursor.moveToFirst()) {
@@ -28,9 +36,21 @@ public class MainActivity extends AppCompatActivity {
             int descriptionIndex = cursor.getColumnIndex("description");
             int idIndex = cursor.getColumnIndex("id");
             do {
-                System.out.println(cursor.getString(nameIndex));
+                dataList.add(new DataModel(cursor.getString(nameIndex), cursor.getString(descriptionIndex)));
+                setAdapter();
             } while (cursor.moveToNext());
         } else
             cursor.close();
+    }
+
+    private void initRecyclerView() {
+        rv = findViewById(R.id.rv);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        rv.setLayoutManager(llm);
+    }
+
+    private void setAdapter() {
+        RVAdapter adapter = new RVAdapter(dataList, this);
+        rv.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/com/stdio/dictionary/RVAdapter.java
@@ -0,0 +1,72 @@
+package com.stdio.dictionary;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class RVAdapter extends RecyclerView.Adapter<RVAdapter.DataViewHolder> {
+
+    public static class DataViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tvTitle;
+
+        DataViewHolder(View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.text);
+        }
+    }
+
+    List<DataModel> dataList;
+    Context mContext;
+
+    RVAdapter(List<DataModel> dataList, Context context) {
+        this.dataList = dataList;
+        this.mContext = context;
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    @Override
+    public DataViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.recycler_item, viewGroup, false);
+        DataViewHolder pvh = new DataViewHolder(v);
+        return pvh;
+    }
+
+    @Override
+    public void onBindViewHolder(DataViewHolder dataViewHolder, final int position) {
+        dataViewHolder.tvTitle.setText(dataList.get(position).title);
+        dataViewHolder.tvTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(mContext, dataList.get(position).title, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position;
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+}
